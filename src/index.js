16b1423e540c6d887cacf9b686ad790c7b39e0d6
Ilya Agarkov <ilya.a@devvela.com>
2015-11-13 00:20:22 +0500

    rc1

---
+++ src/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+function reactBemClasses(Component, options){
+
+  const {block, modifiers} = options;
+
+  Component.prototype.block = function(passed = {}){
+    let classesSet = [];
+    if(this.props.className){
+      classesSet.push(this.props.className);
+    }
+
+    classesSet.push(block);
+
+    const
+      modifiersFromProps = (modifiers || [])
+        .filter(modifierName=>!!this.props[modifierName])
+        .map(modifierName => createModifier(block, modifierName, this.props[modifierName])),
+      modifiersFromArguments = modifiersFromObj(block, passed);
+
+    classesSet.push(...modifiersFromProps, ...modifiersFromArguments);
+
+    return classesSet.join(' ');
+  };
+
+  Component.prototype.element = function(element, modifiers){
+    let classesSet = [];
+    const
+      elementClass = `${block}__${element}`,
+      modifiersClasses = modifiersFromObj(elementClass, modifiers);
+
+    classesSet.push(elementClass, ...modifiersClasses);
+    return classesSet.join(' ');
+  };
+
+  return Component;
+}
+
+
+function modifiersFromObj(baseClass, modifiers){
+  let modifiersClasses = [];
+  for(const modifier in modifiers){
+    modifiersClasses.push(createModifier(baseClass, modifier, modifiers[modifier]));
+  }
+  return modifiersClasses;
+}
+
+function createModifier(baseClass, modifierName, modifierValue){
+  if(!modifierValue || modifierValue === false) return '';
+  let className = `${baseClass}--${modifierName}`;
+  if(modifierValue && modifierValue !== true){
+    className += `-${modifierValue}`;
+  }
+  return resultClass;
+}
+
+module.exports = reactBemClasses;
